3fc1b8799dc92b716401db10cae4db229a3fbe6a
fathurrohmat <110435548+fathurrohmat@users.noreply.github.com>
2022-11-01 10:26:53 +0900

    fix session

--- application/controllers/Jurusan.php
+++ application/controllers/Jurusan.php
@@ -1,44 +1,45 @@
 <?php
-class Jurusan extends CI_Controller{
-	function __construct(){
+class Jurusan extends CI_Controller
+{
+	function __construct()
+	{
 		parent::__construct();
 		$this->load->model('model_jurusan');
-		
 	}
-	function index(){
-		$jum=$this->model_jurusan->get_jurusan_home_aja();
-        $page=$this->uri->segment(3);
-        if(!$page):
-            $offset = 0;
-        else:
-            $offset = $page;
-        endif;
-        $limit=4;
-        $config['base_url'] = base_url() . 'jurusan/index/';
-            $config['total_rows'] = $jum->num_rows();
-            $config['per_page'] = $limit;
-            $config['uri_segment'] = 3;
-						//Tambahan untuk styling
-						$config['full_tag_open'] = '<ul class="pagination-center">';
-						$config['full_tag_close'] = '</ul>';
-						$config['num_tag_open'] = '<li>';
-						$config['num_tag_close'] = '</li>';
-						$config['cur_tag_open'] = '<li class="active"><a href="#">';
-						$config['cur_tag_close'] = '</a></li>';
-						$config['prev_tag_open'] = '<li>';
-						$config['prev_tag_close'] = '</li>';
-						$config['next_tag_open'] = '<li>';
-						$config['next_tag_close'] = '</li>';
-						$config['last_tag_open'] = '<li>';
-						$config['last_tag_open'] = '<li>';
-						$config['first_tag_open'] = '<li>';
-						$config['first_tag_open'] = '<li>';
-            $this->pagination->initialize($config);
-            $x['page'] =$this->pagination->create_links();
-						$x['data']=$this->model_jurusan->jurusan_perpage($offset,$limit);
-						$x['header'] = $this->load->view('tampilan_frontend/header','',TRUE);
-			
-						$this->load->view('tampilan_frontend/view_jurusan',$x);
-	}
+	function index()
+	{
+		$jum = $this->model_jurusan->get_jurusan_home_aja();
+		$page = $this->uri->segment(3);
+		if (!$page) :
+			$offset = 0;
+		else :
+			$offset = $page;
+		endif;
+		$limit = 4;
+		$config['base_url'] = base_url() . 'jurusan/index/';
+		$config['config_total_rows'] = $jum->num_rows();
+		$config['per_page'] = $limit;
+		$config['uri_segment'] = 3;
+		//Tambahan untuk styling
+		$config['full_tag_open'] = '<ul class="pagination-center">';
+		$config['full_tag_close'] = '</ul>';
+		$config['num_tag_open'] = '<li>';
+		$config['num_tag_close'] = '</li>';
+		$config['cur_tag_open'] = '<li class="active"><a href="#">';
+		$config['cur_tag_close'] = '</a></li>';
+		$config['prev_tag_open'] = '<li>';
+		$config['prev_tag_close'] = '</li>';
+		$config['next_tag_open'] = '<li>';
+		$config['next_tag_close'] = '</li>';
+		$config['last_tag_open'] = '<li>';
+		$config['last_tag_open'] = '<li>';
+		$config['first_tag_open'] = '<li>';
+		$config['first_tag_open'] = '<li>';
+		$this->pagination->initialize($config);
+		$x['page'] = $this->pagination->create_links();
+		$x['data'] = $this->model_jurusan->jurusan_perpage($offset, $limit);
+		$x['header'] = $this->load->view('tampilan_frontend/header', '', TRUE);
 
+		$this->load->view('tampilan_frontend/view_jurusan', $x);
+	}
 }
